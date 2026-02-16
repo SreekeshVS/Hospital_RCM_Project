e2ab87c089eb8f8c766faaa9fe15641b61fe07d2
SreekeshVS <vssreekesh@gmail.com>
2026-02-16 13:11:46 +0530

    Correcting the sql file path for BigQuery jobs

--- Workflows/Data_processing_Dag.py
+++ Workflows/Data_processing_Dag.py
@@ -6,12 +6,10 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobO
 
 #airflow Configurations
 PROJECT_ID = "project-b2d1202e-c674-4674-a8a"
-REGION = "us-central1"
-CLUSTER_NAME = "my-hospital-dp-cluster"
-COMPOSER_BUCKET = "us-central1-hospital-rcm-co-cc2246f3-bucket"
-LANDING_BRONZE_SRC = f"gs://{COMPOSER_BUCKET}/data/DataProcessing/Landing_to_Bronze.sql"
-BRONZE_SILVER_SRC = f"gs://{COMPOSER_BUCKET}/data/DataProcessing/Bronze_to_Silver.sql"
-SILVER_GOLD_SRC = f"gs://{COMPOSER_BUCKET}/data/DataProcessing/Silver_to_Gold.sql"
+REGION = "US"
+LANDING_BRONZE_SRC = "/home/airflow/gcs/data/DataProcessing/Landing_to_Bronze.sql"
+BRONZE_SILVER_SRC = "/home/airflow/gcs/data/DataProcessing/Bronze_to_Silver.sql"
+SILVER_GOLD_SRC = "/home/airflow/gcs/data/DataProcessing/Silver_to_Gold.sql"
 
 #Read the SQL file from SRC
 def read_sql_file(sql_file):
